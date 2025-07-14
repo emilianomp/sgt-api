d8ff6d69cb71f9e6546cc002cb4c141f08f7ac6b
EmilianoDEX <emiliano@dex360.net>
2025-07-14 01:04:48 -0300

    Fix json

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,7 +5,7 @@ const taskController = require('../controllers/taskController');
 router.get('/', taskController.getAllTasks);
 router.post('/', taskController.createTask);
 router.post('/', taskController.getTaskById);
-router.post('/', taskController.updateTask);
-router.post('/', taskController.deleteTask);
+// router.post('/', taskController.updateTask);
+// router.post('/', taskController.deleteTask);
 
 module.exports = router;
